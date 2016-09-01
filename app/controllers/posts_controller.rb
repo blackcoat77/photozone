b9d3a78d993ad46bcd7a4135bb1f9025ccec15ba
blackcoat77 <kimanovic@gmail.com>
2016-09-01 13:32:02 -0400

    Add Paperclip gem attachment

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,31 @@
 class PostsController < ApplicationController
+
   def index
+    @posts = Post.all
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
+    @post = Post.new
   end
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:success] = "The post has been successfully created!"
+      redirect_to post_path(@post)
+    else
+      flash[:error] = @post.errors.full_messages
+      redirect_to new_post_path
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:image, :description)
+  end
+
 end
