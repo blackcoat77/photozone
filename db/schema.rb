04b8db4fc92c1fa955b34b55eb357323dec56129
blackcoat77 <kimanovic@gmail.com>
2016-09-01 11:42:55 -0400

    Configure routes, and add user register/login/logout links to main layout

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160901141408) do
+ActiveRecord::Schema.define(version: 20160901145751) do
+
+  create_table "posts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.text     "description", limit: 65535
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "email",                  default: "", null: false
