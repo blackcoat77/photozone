9861779dda681646dcfef607218a59aeb3476c9a
blackcoat77 <kimanovic@gmail.com>
2016-09-09 12:29:21 -0400

    Add devise views for User model, fields in Users table, strong params for editing users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+    protect_from_forgery with: :exception
+
+    # Strong parameters for devise go below
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email, :password, :current_password, :is_female, :date_of_birth])
+    end
+
+
 end
