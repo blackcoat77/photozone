ca1d830553727e7b62eb75b342fa925f408f2219
blackcoat77 <kimanovic@gmail.com>
2016-11-11 16:41:08 -0500

    Adds gem figaro, dropbox.yml config file

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,7 +10,28 @@ class Post < ApplicationRecord
   validates :image, presence: :true
   validates :category, presence: :true
 
-  has_attached_file :image, styles: { large: "450x450#", medium: "300x300#", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  if Rails.env.development?
+       has_attached_file :image,
+
+       styles: { large: "450x450#", medium: "300x300#", thumb: "100x100>" },
+       default_url: "/images/:style/missing.png"
+      #  url: '/assets/users/:id/:style/:basename.:extension',
+      #  path: ':rails_root/public/assets/users/:id/:style/:basename.:extension'
+
+    else
+    # Add dropbox config
+    has_attached_file :image,
+
+                      :storage => :dropbox,
+                      :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+                      :styles => { large: "450x450#", medium: "300x300#", thumb: "100x100>" }
+                         
+
+                      # url: '/assets/admin_user/:id/:style/:basename.:extension',
+                      # path: ':rails_root/public/assets/admin_user/:id/:style/:basename.:extension'
+  end
+
+  # has_attached_file :image, styles: { large: "450x450#", medium: "300x300#", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
 end
